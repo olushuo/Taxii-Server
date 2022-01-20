7082dd2a37c4a960c90774ec9918be007947f068
olushuo <iamlushu@gmail.com>
2022-01-19 18:30:52 -0800

    Setup a cit-taxii-server for testing

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 sudo docker image pull olushuo/cti-taxii-server:1.0
-sudo docker container run --name cti-taxii-server -d -p 5000:500 olushuo/cti-taxii-server:1.0
+sudo docker container run --name cti-taxii-server -d -p 5000:5000 olushuo/cti-taxii-server:1.0
